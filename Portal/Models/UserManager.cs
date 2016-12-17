a6fd049c56f84b66d6dd4de66e1afe3b594091c7
evglaptev <evglaptev23@gmail.com>
2016-12-17 21:07:10 +0300

    Add new classes
    Add User, context , UserManager classes

---
+++ Portal/Models/UserManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace Portal.Models
+{
+    public class UserManager : UserManager<User>
+    {
+        public UserManager(IUserStore<User> store)
+                : base(store)
+        {
+        }
+        public static UserManager Create(IdentityFactoryOptions<UserManager> options,
+                                                IOwinContext context)
+        {
+            ApplicationContext db = context.Get<ApplicationContext>();
+            UserManager manager = new UserManager(new UserStore<User>(db));
+            return manager;
+        }
+    }
+}
